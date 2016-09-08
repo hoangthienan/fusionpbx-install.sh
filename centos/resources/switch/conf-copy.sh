d85a0d70c8a1248d5678664ca84e8178402d29a1
An <hoangthienan@gmail.com>
2016-09-08 21:45:19 +0700

    Copy the switch conf files to /etc/freeswitch

--- centos/resources/switch/conf-copy.sh
+++ centos/resources/switch/conf-copy.sh
@@ -6,8 +6,8 @@ verbose "Copy the switch conf files to /etc/freeswitch"
 
 if [ -d /etc/freeswitch ]; then
 	verbose "Directory /etc/freeswitch not empty"
-else
+	rm -rf /etc/freeswitch.org
 	mv /etc/freeswitch /etc/freeswitch.org
-	mkdir /etc/freeswitch
 fi
+mkdir /etc/freeswitch
 cp -R /var/www/fusionpbx/resources/templates/conf/* /etc/freeswitch
